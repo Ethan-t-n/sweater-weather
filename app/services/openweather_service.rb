f45a79cf8ae769ac541bde9a969cc6a519707bbb
Ethan Nguyen <etnguyen16@gmail.com>
2022-09-29 08:28:15 -0600

    change 3.0 to 2.5

--- app/services/openweather_service.rb
+++ app/services/openweather_service.rb
@@ -1,7 +1,7 @@
 class OpenweatherService 
 
     def self.get_weather(coordinates)
-        response = BaseService.open_weather_conn.get("/data/3.0/onecall?lat=#{coordinates.latitude}&lon=#{coordinates.longitude}&appid=#{ENV['openweather_api_key']}&units=imperial")
+        response = BaseService.open_weather_conn.get("/data/2.5/onecall?lat=#{coordinates.latitude}&lon=#{coordinates.longitude}&appid=#{ENV['openweather_api_key']}&units=imperial")
         BaseService.get_json(response)
     end
 
